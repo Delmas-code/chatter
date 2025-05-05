d5f35030bff23f826acbc2e518c48239a5869c9e
Delmas-code <58811361+Delmas-code@users.noreply.github.com>
2025-05-05 03:29:04 -0400

    port change

--- app.py
+++ app.py
@@ -296,5 +296,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # socketio.run(app, debug=True, host='0.0.0.0', port=10000)
-    socketio.run(app, debug=True, host='127.0.0.1', port=5000)
+    socketio.run(app, debug=True, host='0.0.0.0', port=10000)
+    # socketio.run(app, debug=True, host='127.0.0.1', port=5000)
